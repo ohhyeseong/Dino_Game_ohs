851ba9669afd5cea22eeaf1533c280cf881fc282
ohhyeseong <ohs020105@gmail.com>
2024-12-18 00:58:04 +0900

    [add] 주석 처리 추가

--- src/init/assets.js
+++ src/init/assets.js
@@ -7,13 +7,14 @@ import { fileURLToPath } from 'url';
 // fileURLToPath는 URL 문자열을 파일 시스템의 경로로 변환
 
 // 현재 파일의 절대 경로. 이 경로는 파일의 이름을 포함한 전체 경로
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url); // 현재 파일의 절대 경로
 
 // path.dirname() 함수는 파일 경로에서 디렉토리 경로만 추출 (파일 이름을 제외한 디렉토리의 전체 경로)
-const __dirname = path.dirname(__filename);
-const basePath = path.join(__dirname, '../../assets');
+const __dirname = path.dirname(__filename); // 현재 파일이 속한 디렉토리
+const basePath = path.join(__dirname, '../../assets'); // 최상위 폴더 + 에셋 폴더로의 절대 경로
 let gameAssets = {}; // 전역함수로 선언
-
+// 파일 읽는 함수
+// 비동기 병렬로 파일을 읽는다.(다수의 파일을 동시에 읽는다.)
 const readFileAsync = (filename) => {
   return new Promise((resolve, reject) => {
     fs.readFile(path.join(basePath, filename), 'utf8', (err, data) => {
